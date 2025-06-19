5522447ab1b084f8e41ecf6cd04bd61aa832e471
ColdIce0816 <alvinleowyankai@gmail.com>
2025-06-19 11:33:23 +0800

    feat: configure devServer settings in vue.config.js

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: "0.0.0.0",
     port: 8082,
+    allowedHosts: "all",
     proxy: {
       "/api": {
         target: "http://localhost:8081",
